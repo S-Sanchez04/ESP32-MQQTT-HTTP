c8a82fa309f28d034418694882da8e1d9cdd982e
estemen27 <estebanbe454545@gmail.com>
2025-10-25 22:22:38 -0500

    Añade primera version de simulacion de lectura para sensor HC-SR04

---
+++ config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define WIFI_SSID "Santi's Iphone"
+#define WIFI_PASS "farra2abril"
+
+#define MQTT_BROKER "broker.hivemq.com"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC_CMD "carroESP32/instrucciones"
+#define MQTT_TOPIC_SENSOR "carroESP32/sensor/hcsr04"
+
+#define HTTP_PORT 80
+#define DEVICE_NAME "esp32-chucho"
+
+#define ENA_PIN 25
+#define IN1_PIN 26
+#define IN2_PIN 27
+#define ENB_PIN 14
+#define IN3_PIN 12
+#define IN4_PIN 13
+
+#define TRIG_PIN 35
+#define ECHO_PIN 34
+
+#define SENSOR_PUB_PERIOD_MS 1000UL
